2147a73765746b29a406654978149a7b3755a570
zaina <zaina@Yazen-HP-Labtop.fios-router.home>
2023-07-29 23:56:52 -0400

    hello

--- src/day4/DemoDay4.java
+++ src/day4/DemoDay4.java
@@ -4,4 +4,7 @@ public class DemoDay4 {
 public static void main(String[] args) {
 	System.out.println("what is the weather today?");
 }
+public static void  getINfo() {
+	System.out.println("ali ahmed");
+}
 }
